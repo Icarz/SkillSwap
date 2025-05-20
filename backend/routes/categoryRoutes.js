4d944758e3e826badbcbffb1bbd76dd8b509f318
Icarz <mr.rghay@email.com>
2025-05-19 22:23:06 -0300

    admin_off

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -6,13 +6,13 @@ const {
   createCategory
 } = require("../controllers/categoryController");
 const authMiddleware = require("../middleware/authMiddleware");
-const adminMiddleware = require("../middleware/adminMiddleware"); // You'll need to create this
+// const adminMiddleware = require("../middleware/adminMiddleware"); // You'll need to create this
 
 // Public routes
 router.get("/", getAllCategories);
 router.get("/:categoryId/skills", getSkillsByCategory);
 
 // Admin-protected routes
-router.post("/", authMiddleware, adminMiddleware, createCategory);
+router.post("/", authMiddleware, createCategory);
 
 module.exports = router;
